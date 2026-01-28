9aa5dbb66dbb3ad82ed2a3d8470acf661abca434
abdulbasheeth <basheethabdul1024@gmail.com>
2026-01-28 12:13:26 +0400

    Setup Vite + Tailwind + React 18 + Drei/Fiber for production build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
